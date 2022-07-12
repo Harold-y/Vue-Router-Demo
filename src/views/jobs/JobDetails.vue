3a3b4d999145224b30ac94104cd175df1dc3b618
Ye <Yixuan.Ye@Dell.com>
2022-07-12 09:48:24 -0500

    cr: demo v1.0.0

---
+++ src/views/jobs/JobDetails.vue
@@ -0,0 +1,36 @@
+<template>
+  <div v-if="job">
+    <h1>{{ job.title }}</h1>
+    <!-- <p>The job id is {{ $route.params.id }}</p> -->
+    <p>The job id is {{ id }}</p>
+    <p>{{ job.details }}</p>
+  </div>
+  <div v-else>
+    <p>Loading job details...</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['id'],
+  //data() {
+  //  return {
+  //    id: this.$route.params.id
+  //  }
+  //}
+  data() {
+    return {
+      BASE_URL: process.env.VUE_APP_BASEURL,
+      job: null,
+    }
+  },
+  // fetch the data and pass into jobs
+  mounted() {
+    fetch(this.BASE_URL + "/jobs/" + this.id).then(res => res.json()).then(data => this.job = data).catch(err => console.log(err.message))
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/jobs/Jobs.vue
@@ -0,0 +1,41 @@
+<template>
+  <h1>Jobs</h1>
+  <div v-for="job in jobs" :key="job.id" class="job">
+    <router-link :to="{ name: 'jobDetails', params: { id: job.id } }">
+      <h2>{{ job.title }}</h2>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      BASE_URL: process.env.VUE_APP_BASEURL,
+      jobs: [],
+    }
+  },
+  // fetch the data and pass into jobs
+  mounted() {
+    fetch(this.BASE_URL + "/jobs").then(res => res.json()).then(data => this.jobs = data).catch(err => console.log(err.message))
+  }
+}
+</script>
+
+<style>
+ .job h2{
+    background: #f4f4f4;
+    padding: 20px;
+    border-radius: 10px;
+    margin: 10px auto;
+    max-width: 600px;
+    cursor: pointer;
+    color: #444;
+  }
+  .job h2:hover {
+    background: #ddd;
+  }
+  .job a{
+    text-decoration: none;
+  }
+</style>
